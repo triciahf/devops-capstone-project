5c38cbe38c75436fc042da7b7606ece2540eea21
triciahf <triciahf.cuentas@gmail.com>
2025-02-23 11:39:56 -0500

    Added security headers

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -55,10 +55,10 @@ class TestAccountService(TestCase):
         """Runs once after each test case"""
         db.session.remove()
 
+
     ######################################################################
     #  H E L P E R   M E T H O D S
     ######################################################################
-
     def _create_accounts(self, count):
         """Factory method to create accounts in bulk"""
         accounts = []
